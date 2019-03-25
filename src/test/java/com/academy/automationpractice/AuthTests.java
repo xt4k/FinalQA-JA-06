1d4e7ebce4b7eb1c8ca476bfc4111c5c1c9fdd6d
Oleg <oleg.kh81@gmail.com>
2019-03-25 19:37:44 +0200

    Lesson 16 -> TestNG

--- src/test/java/com/academy/automationpractice/AuthTests.java
+++ src/test/java/com/academy/automationpractice/AuthTests.java
@@ -4,9 +4,7 @@ import com.academy.automationpractice.model.AuthData;
 import org.openqa.selenium.*;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
-import org.testng.annotations.AfterClass;
-import org.testng.annotations.BeforeClass;
-import org.testng.annotations.Test;
+import org.testng.annotations.*;
 
 import java.io.FileReader;
 import java.io.IOException;
@@ -19,7 +17,7 @@ import static org.testng.Assert.assertEquals;
 import static org.testng.Assert.fail;
 
 public class AuthTests {
-    private String commonProperties = "D:/programming/teaching/qa-ja-06/QA-JA-06/src/main/resources/common.properties";
+    private String commonProperties = "D:/programming/java/QA-JA-06/maven/src/main/resources/common.properties";
     private WebDriver driver;
     private String baseUrl;
 
@@ -27,25 +25,37 @@ public class AuthTests {
 
     private StringBuffer verificationErrors = new StringBuffer();
 
+    @Parameters("browser")
     @BeforeClass(alwaysRun = true)
-    public void setUp() throws Exception {
-        initDrivers();
+    public void setUp(@Optional("firefox") String browser) throws Exception {
+        initDrivers(browser);
 
         baseUrl = "http://automationpractice.com/index.php";
         driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
     }
 
-    private void initDrivers() {
+    private void initDrivers(String browser) {
         Properties properties = new Properties();
         try {
             properties.load(new FileReader(commonProperties));
         } catch (IOException e) {
             e.printStackTrace();
         }
-        System.setProperty("webdriver.chrome.driver", properties.getProperty("chrome.driver"));
-        System.setProperty("webdriver.gecko.driver", properties.getProperty("gecko.driver"));
-//        driver = new ChromeDriver();
-        driver = new FirefoxDriver();
+
+        switch (browser) {
+            case "chrome":
+                System.setProperty("webdriver.chrome.driver", properties.getProperty("chrome.driver"));
+                driver = new ChromeDriver();
+                break;
+
+            case "firefox":
+                System.setProperty("webdriver.gecko.driver", properties.getProperty("gecko.driver"));
+                driver = new FirefoxDriver();
+                break;
+
+            default:
+                throw new IllegalArgumentException("Unknown browser " + browser);
+        }
     }
 
     @Test
