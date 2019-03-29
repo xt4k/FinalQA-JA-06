a8f76239f90d76e08d3c229f0d92eaf38c283b36
Oleg <oleg.kh81@gmail.com>
2019-03-29 20:15:07 +0200

    Lesson 17 -> Rozetka

---
+++ src/test/java/com/academy/rozetka/AuthTests.java
@@ -0,0 +1,11 @@
+package com.academy.rozetka;
+
+import org.testng.annotations.Test;
+
+public class AuthTests {
+
+    @Test
+    public void testSuccessAuth() {
+        System.out.println("***Rozetka test Auth start***");
+    }
+}
